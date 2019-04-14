eddcfa7e5589fbd85895660d8c78ae8477491136
XSA_DEV <dev@dev.com>
2019-04-14 14:27:29 +0000

    initial

---
+++ srv/router/index.js
@@ -0,0 +1,8 @@
+/*eslint-env node, es6 */
+"use strict";
+
+module.exports = (app, server) => {
+	app.use("/node", require("./routes/myNode")());
+	app.use("/node/ex2", require("./routes/ex2")());
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+};
